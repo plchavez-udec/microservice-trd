d0503c454aab669323f2e8df1b4fd88a696b82fd
plchavez-udec <plchavez@ucundinamarca.edu.co>
2021-04-14 11:15:41 -0500

    Cambios

--- src/main/java/co/edu/ierdminayticha/sgd/trds/TrdsApplication.java
+++ src/main/java/co/edu/ierdminayticha/sgd/trds/TrdsApplication.java
@@ -18,15 +18,12 @@ public class TrdsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrdsApplication.class, args);
 	}
-	
+
 	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
-    }
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 
 	@Bean
 	public ModelMapper modelMapper() {
