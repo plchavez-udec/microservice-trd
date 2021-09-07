2419173ca989d46fd185c5679001d32c2e7f3a4f
Pedro Luis <plchavez@ucundinamarca.edu.co>
2021-09-07 16:01:47 -0500

    Ajuste error generico

--- src/main/java/co/edu/ierdminayticha/sgd/trds/util/ResponseCodeConstants.java
+++ src/main/java/co/edu/ierdminayticha/sgd/trds/util/ResponseCodeConstants.java
@@ -2,9 +2,9 @@ package co.edu.ierdminayticha.sgd.trds.util;
 
 public class ResponseCodeConstants {
 
-	public static final String ERROR_TECHNICAL_TRANSACTION_PROCESSING = "gdmt.trd.error-tecnico.procesamiento-tx";
+	public static final String ERROR_TECHNICAL_TRANSACTION_PROCESSING = "gdmt.generico.error-tecnico.procesamiento-tx";
 
-	public static final String ERROR_TECHNICAL_INPUT_FILE_VALIDATION = "gdmt.trd.error-tecnico.validacion-campos-entrada";
+	public static final String ERROR_TECHNICAL_INPUT_FILE_VALIDATION = "gdmt.generico.error-tecnico.validacion-campos-entrada";
 
 	public static final String ERROR_BUSINESS_TRD_ALREADY_EXIST = "gdmt.trd.error-negocio.trd-existente";
 
